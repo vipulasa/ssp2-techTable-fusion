f27ee82a29e1cec7b83288bcad283b7e29fc724c
VIpula S <v.s.anandapiya@gmail.com>
2024-07-30 15:06:04 +0530

    Add QR code generation route
    This commit introduces a new route '/qr-sample/{id?}' that generates a QR code based on the provided ID. It utilizes the `BaconQrCode` library to create and render the QR code as an SVG string.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use BaconQrCode\Renderer\Color\Rgb;
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\Fill;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin-guard', function () {
@@ -30,6 +36,18 @@ Route::middleware([
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/qr-sample/{id?}', function ($id) {
+    $svg = (new Writer(
+        new ImageRenderer(
+            new RendererStyle(192, 0, null, null, Fill::uniformColor(new Rgb(255, 255, 255), new Rgb(45, 55, 72))),
+            new SvgImageBackEnd
+        )
+    ))->writeString('Adoo - ' . $id);
+
+    return trim(substr($svg, strpos($svg, "\n") + 1));
 });
 
 Route::middleware([
